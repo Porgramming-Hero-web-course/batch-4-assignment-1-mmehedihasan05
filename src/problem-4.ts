3a83143f079a99008b7cb12d041374e93894be8b
Md. Mehedi Hasan <mmehedi.hasan229@gmail.com>
2024-11-08 11:02:17 +0600

    Problem 4 solved

---
+++ src/problem-4.ts
@@ -0,0 +1,25 @@
+// type of Circle
+type Circle = { shape: "circle"; radius: number };
+
+// type of Rectangle
+type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+};
+
+// Union of Circle and Rectangle
+type Shape = Circle | Rectangle;
+
+// Calculatoion
+const calculateShapeArea = (value: Shape): number => {
+    if (value.shape === "circle") {
+        let circleArea = Math.PI * value.radius * value.radius;
+        return circleArea;
+    } else if (value.shape === "rectangle") {
+        let rectangleArea = value.width * value.height;
+        return rectangleArea;
+    } else {
+        return 0;
+    }
+};
